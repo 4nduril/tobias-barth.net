8ef1479dca1a2a8ff09a555e1cc99832b4fa1731
Tobias Barth <mail@tobias-barth.net>
2021-02-26 15:02:42 +0100

    Add Anne, DSE and small styles

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const MainContent: FC = ({ children }) => (
-  <main className="mx-auto max-w-prose">{children}</main>
+  <main className="mx-auto mb-7 max-w-prose px-4 xl:px-0">{children}</main>
 );
 
 export default MainContent;
--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -19,13 +19,21 @@ const SiteFooter: FC = () => (
     </p>
     <p className="mb-7">
       <Link href="/dse">
-        <a className="inline-block px-4 py-2 text-link-color hover:bg-link-color hover:text-gray-200">
+        <a className="inline-block px-4 py-2 text-link-color hover:bg-link-color hover:text-gray-200 transition-colors">
           Datenschutzerklärung
         </a>
       </Link>
     </p>
     <p className="mb-7">
       Postadresse: Tobias Barth, Türrschmidtstr. 35, 10317 Berlin
+    </p>
+    <p className="mb-7">
+      <a
+        className="inline-block px-4 py-2 text-link-color hover:bg-link-color hover:text-gray-200 transition-colors"
+        href="https://annebarth.de"
+      >
+        Fotos von Anne Barth
+      </a>
     </p>
   </footer>
 );
