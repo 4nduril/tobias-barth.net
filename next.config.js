336a5ce32479e7dbba8b720ed8d8efecab03440a
Tobias Barth <mail@tobias-barth.net>
2021-03-03 09:24:17 +0100

    Redirect from old blog urls

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-	webpack: function (config) {
-		config.module.rules.push({
-			test: /\.md$/,
-			use: 'raw-loader',
-		})
-		return config
-	}
-}
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/blog/:year/:month/:slug",
+        destination: "/blog/:slug",
+        permanent: true,
+      },
+    ];
+  },
+};
