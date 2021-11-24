74e7a85bbfa2e5fd8f945559b1604358c515dda8
Tobias Barth <mail@tobias-barth.net>
2021-11-24 18:17:25 +0100

    feat(analytics): use next-plausible

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
-module.exports = {
+const { withPlausibleProxy } = require('next-plausible')
+
+module.exports = withPlausibleProxy()({
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader",
-    });
-    return config;
+      use: 'raw-loader',
+    })
+    return config
   },
   async redirects() {
     return [
       {
-        source: "/blog/:year/:month/:slug",
-        destination: "/blog/:slug",
+        source: '/blog/:year/:month/:slug',
+        destination: '/blog/:slug',
         permanent: true,
       },
-    ];
+    ]
   },
-};
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import React, { FC } from 'react'
 import { AppProps } from 'next/app'
 import '../styles/global.css'
 import { ThemeProvider, theme } from '../utils/theme'
+import PlausibleProvider from 'next-plausible'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <ThemeProvider value={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
+  <PlausibleProvider domain="tobias-barth.net">
+    <ThemeProvider value={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </PlausibleProvider>
 )
 
 export default App
