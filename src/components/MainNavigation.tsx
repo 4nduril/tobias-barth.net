aa1cf0c01347e462d6bc564ab67804e3474a6755
Tobias Barth <mail@tobias-barth.net>
2022-11-24 16:20:56 +0100

    feat: measure index page render (#234)
    * feat: measure index page render
    
    * fix: use new Link without a

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -16,13 +16,12 @@ const MainNavigation: FunctionComponent = () => {
           </Link>
 		</li> */}
         <li className="flex-grow mx-8 text-center">
-          <Link href="/blog">
-            <a
-              className="inline-block text-gray-200 no-underline px-4 py-2 hover:bg-link-color transition-colors"
-              title="Mein Blog über Webentwicklung und Ähnliches"
-            >
-              Blog
-            </a>
+          <Link
+            href="/blog"
+            className="inline-block text-gray-200 no-underline px-4 py-2 hover:bg-link-color transition-colors"
+            title="Mein Blog über Webentwicklung und Ähnliches"
+          >
+            Blog
           </Link>
         </li>
       </ul>
