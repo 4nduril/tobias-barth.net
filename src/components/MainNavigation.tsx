5e2c0c3249f139701ed70cedc0c1739a7b86e1d5
Tobias Barth <mail@tobias-barth.net>
2025-05-19 11:05:40 +0200

    chore(tailwindcss): upgrade all configuration for v4

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -5,7 +5,7 @@ const MainNavigation: FunctionComponent = () => {
   return (
     <nav className="mb-7 pb-1 font-bold bg-primary">
       <ul className="flex justify-around list-none">
-        {/* <li className="flex-grow mx-8 text-center">
+        {/* <li className="grow mx-8 text-center">
           <Link href="/portfolio">
             <a
               className="inline-block text-gray-200 no-underline px-4 py-2 hover:bg-link-color transition-colors"
@@ -15,7 +15,7 @@ const MainNavigation: FunctionComponent = () => {
             </a>
           </Link>
 		</li> */}
-        <li className="flex-grow mx-8 text-center">
+        <li className="grow mx-8 text-center">
           <Link
             href="/blog"
             className="inline-block text-gray-200 no-underline px-4 py-2 hover:bg-link-color transition-colors"
